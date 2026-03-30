4e1cfcb14cefe8a1c2b13fa500b99903173c3f7b
Sheyswaggz <oluwaseun65771@gmail.com>
2026-03-30 08:29:14 +0100

    Auto-merge: chore(delivery): deploy landing-page
    Automatically merged PR for task None

--- postcss.config.js
+++ postcss.config.js
@@ -29,7 +29,7 @@ module.exports = {
           },
           mergeRules: true,
           cssDeclarationSorter: {
-            order: 'alphabetically'
+        order: 'alphabetical',
           }
         }
       ]
